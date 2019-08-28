cdf971746f5564de9eca2229f7c04faec9a3ed94
qH0sT <asuslaptop230@gmail.com>
2019-08-28 13:07:07 +0300

    Yılan oyunu

---
+++ yilaaaannn/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace yilaaaannn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(241, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(226, 153);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.linkLabel2);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 316);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(889, 109);
+            this.panel1.TabIndex = 20;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 94);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yem yeme hassasiyeti";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 39);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 25;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(418, 87);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(70, 13);
+            this.linkLabel2.TabIndex = 23;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Tekrar Başlat";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(165, 87);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(103, 13);
+            this.linkLabel1.TabIndex = 22;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Coded by 20071999";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(811, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 29);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "D";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(775, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 29);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(734, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 29);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(769, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 29);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "W";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(165, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Puan: 0";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pictureBox4.Location = new System.Drawing.Point(490, 107);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox4.TabIndex = 21;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 425);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yılan oyunu 1.0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ yilaaaannn/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace yilaaaannn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pcbx.Add(pictureBox2);
+            pcbx.Add(pictureBox3);
+        }
+        List<PictureBox> pcbx = new List<PictureBox>();
+        int puan = 0;
+        string yon = "sago go go";
+        PictureBox pbx = new PictureBox();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            switch (yon)
+            {
+                case "sag":
+                    Saga_git();
+                    break;
+
+                case "assagi":
+                    Assagi_git();
+                    break;
+
+                case "yukari":
+                    Yukari_git();
+                    break;
+
+                case "sol":
+                    Sola_git();
+                    break;
+
+            }
+
+        }
+
+
+        private void Yukari_git()
+        {
+            pictureBox1.Top -= 10;
+            for (int i = 0; i < pcbx.Count; i++)
+            {
+                try
+                {
+                    pcbx[i].Top = pictureBox1.Top + 15;
+                    pcbx[i].Top = pcbx[i - 1].Top + 15;
+                    pcbx[i].Left = pictureBox1.Left;
+
+                }
+                catch (Exception) { pcbx[0].Left = pictureBox1.Left; }
+            }
+        }
+
+        private void Saga_git()
+        {
+            pictureBox1.Left += 10;
+            for (int i = 0; i < pcbx.Count; i++)
+            {
+                try
+                {
+                    pcbx[i].Left = pictureBox1.Left - 15;
+                    pcbx[i].Left = pcbx[i - 1].Left - 15;
+                    pcbx[i].Top = pictureBox1.Top;
+                    Application.DoEvents();
+
+                }
+                catch (Exception) { pcbx[0].Top = pictureBox1.Top; }
+            }
+        }
+
+        private void Sola_git()
+        {
+            pictureBox1.Left -= 10;
+            for (int i = 0; i < pcbx.Count; i++)
+            {
+                try
+                {
+                    pcbx[i].Left = pictureBox1.Left + 15;
+                    pcbx[i].Left = pcbx[i - 1].Left + 15;
+                    pcbx[i].Top = pictureBox1.Top;
+                    Application.DoEvents();
+
+                }
+                catch (Exception) { pcbx[0].Top = pictureBox1.Top; }
+            }
+        }
+
+        private void Assagi_git()
+        {
+            pictureBox1.Top += 10;
+            for (int i = 0; i < pcbx.Count; i++)
+            {
+                try
+                {
+                    pcbx[i].Top = pictureBox1.Top - 15;
+                    pcbx[i].Top = pcbx[i - 1].Top - 15;
+                    pcbx[i].Left = pictureBox1.Left;
+                    Application.DoEvents();
+                }
+                catch (Exception) { pcbx[0].Left = pictureBox1.Left; }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    yon = "yukari";
+                    label2.ForeColor = Color.Green;
+                    break;
+
+                case Keys.S:
+                    yon = "assagi";
+                    label4.ForeColor = Color.Green;
+                    break;
+
+                case Keys.D:
+                    yon = "sag";
+                    label5.ForeColor = Color.Green;
+                    break;
+
+                case Keys.A:
+                    yon = "sol";
+                    label3.ForeColor = Color.Green;
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int bas = pictureBox1.Top;
+            int bas2 = pictureBox1.Left;
+
+            int bas3 = pictureBox4.Top;
+            int bas4 = pictureBox4.Left;
+
+            int sonucX = bas3 - bas;
+            int sonuxY = bas4 - bas2;
+
+            if (sonucX < 0) { sonucX = bas - bas3; }
+            if (sonuxY < 0) { sonuxY = bas2 - bas4; }
+
+            //Text = "PC1 Top: " + pictureBox1.Top.ToString() + " PC4 Top:" + pictureBox4.Top.ToString() + " PC1 Left: " + pictureBox1.Left.ToString() + " PC4 Left: " + pictureBox4.Left.ToString() ;
+
+            if (pictureBox1.Left <= 966 && pictureBox1.Left >= 886 || pictureBox1.Top >= 283 || pictureBox1.Left < 0 || pictureBox1.Top < 0) //Eğer duvarlara çarparsa yanarsınız.
+            {
+                timer1.Stop();
+                timer2.Stop();
+                MessageBox.Show("Yandınız!!", "Yılan oyunu");
+            }
+            else if (sonucX <= Convert.ToInt32(numericUpDown1.Value) || sonuxY <= Convert.ToInt32(numericUpDown1.Value)) //Yem yedik mi ? tam koordinat vermiyorum, yaklaşık kalan 1 olsa yeter.
+            {
+                puan += 1;
+                label1.Text = "Puan: " + puan.ToString();
+                pictureBox4.Left = new Random().Next(0, 866);
+                pictureBox4.Top = new Random().Next(0, 280);
+                PictureBox pbox = new PictureBox();
+                pbox.Location = new Point(418, 87);
+                pbox.BackColor = Color.Red;
+                pbox.Size = pictureBox1.Size;
+                pcbx.Add(pbox);
+                Controls.Add(pbox);
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    yon = "yukari";
+                    label2.ForeColor = SystemColors.ControlText;
+                    break;
+
+                case Keys.S:
+                    yon = "assagi";
+                    label4.ForeColor = SystemColors.ControlText;
+                    break;
+
+                case Keys.D:
+                    yon = "sag";
+                    label5.ForeColor = SystemColors.ControlText;
+                    break;
+
+                case Keys.A:
+                    yon = "sol";
+                    label3.ForeColor = SystemColors.ControlText;
+                    break;
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
